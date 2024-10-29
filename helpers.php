dead23842127e1d6c664d1ec966b07fbeb8f2b7d
bokhuuu <zurabokhua@gmail.com>
2024-10-29 21:42:19 +0400

    Modify loadView helper to accept data, dynamically display listings from database with foreach, and add formatSalary helper for salary formatting

--- helpers.php
+++ helpers.php
@@ -6,11 +6,12 @@ function basePath($path = '')
 }
 
 
-function loadView($name)
+function loadView($name, $data = [])
 {
     $viewPath = basePath("views/{$name}.view.php");
 
     if (file_exists($viewPath)) {
+        extract($data);
         require $viewPath;
     } else {
         echo "View '{$name}' not found!";
@@ -45,4 +46,10 @@ function inspectAndDie($value)
     var_dump($value);
     echo '</pre>';
     die();
+}
+
+
+function formatSalary($salary)
+{
+    return '$' . number_format(floatval($salary));
 }
