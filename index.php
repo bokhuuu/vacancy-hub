e3a82b8ef7f327b89a489009df107e9233e47d45
bokhuuu <zurabokhua@gmail.com>
2024-11-25 12:58:45 +0400

    Set up PSR-4 autoloading for Framework and App namespaces with respective classes and folders

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require __DIR__ . '/vendor/autoload.php';
 require './helpers.php';
-require basePath('Router.php');
-require basename('Database.php');
+
+use Framework\Router;
+
 
 $router = new Router();
 
