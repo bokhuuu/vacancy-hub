23cbdefb7111cc73490e104eb97ac386ae55e120
bokhuuu <zurabokhua@gmail.com>
2024-11-26 16:21:07 +0400

    Implement custom validation methods for string length, email, and matching values

--- App/Controllers/ListingController.php
+++ App/Controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Framework\Database;
+use Framework\Validation;
 
 
 class ListingController
@@ -17,6 +18,7 @@ class ListingController
 
     public function index()
     {
+
         $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
 
         loadView('listings/index', [
