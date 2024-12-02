ffb5308ab1f1ba6d17b5d612d8648bd6cb96010a
bokhuuu <zurabokhua@gmail.com>
2024-12-02 12:51:10 +0400

    Add store method in UserController for user registration with validation, create initial login and registration form views. Additionally, create a separate error partial for views (data insertion into the database is pending)

---
+++ App/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+use Framework\Validation;
+
+class UserController
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+
+        $this->db = new Database($config);
+    }
+
+    public function login()
+    {
+        loadView('users/login');
+    }
+
+    public function create()
+    {
+        loadView('users/create');
+    }
+}
--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -5,8 +5,8 @@
             <a href="/vacancy-hub">Vacancy Hub</a>
         </h1>
         <nav class="space-x-4">
-            <a href="login.html" class="text-white hover:underline">Login</a>
-            <a href="register.html" class="text-white hover:underline">Register</a>
+            <a href="/vacancy-hub/auth/login" class="text-white hover:underline">Login</a>
+            <a href="/vacancy-hub/auth/register" class="text-white hover:underline">Register</a>
             <a
                 href="/vacancy-hub/listings/create"
                 class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
